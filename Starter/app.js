ca0a7435cd66b84031d7abbd38498c0bfe0bb84c
Amabilli Cristine Wizzentainer <115269283+amabillicristine@users.noreply.github.com>
2025-05-14 15:22:14 -0300

    Chamada de API
    Chamada de API Open weather e construção de cards com ng-reapt

--- Starter/app.js
+++ Starter/app.js
@@ -12,9 +12,33 @@ weatherApp.config(function($routeProvider) {
     })
 })
 
-weatherApp.controller('homeController', ['$scope', function ($scope ) {
+weatherApp.service('cityService', function(){
+    this.city = "Joinville"
+})
 
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService ) {
+    $scope.city = cityService.city
+    $scope.$watch('city',function(){
+        cityService.city = $scope.city
+    });
 }])
-weatherApp.controller('forecastController', ['$scope', '$log', function($scope, $log) {
-    
+weatherApp.controller('forecastController', ['$scope', '$http', 'cityService', 
+    function($scope, $http, cityService) {
+    $scope.city = cityService.city;
+
+    $http({
+        method: 'GET',
+        url: "https://api.openweathermap.org/data/2.5/forecast",
+        params: {
+            q: $scope.city,
+            appid: "2c316654d385a821b2bc34c5a57b488b",
+            units: "metric",
+            lang: "pt_br"
+        }
+    }).then(function(response) {
+        $scope.weatherResult = response.data;
+        console.log($scope.weatherResult)
+    }, function(error) {
+        console.error("Error fetching weather data:", error);
+    });
 }])
